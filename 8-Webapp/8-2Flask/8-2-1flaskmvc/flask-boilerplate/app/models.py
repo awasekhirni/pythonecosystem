539bec6d3b11742e0e75571ab171f3b0d0720a1b
Syed Awase Khirni <awasekhirni@gmail.com>
2019-05-29 13:12:57 +0530

    Create models.py

---
+++ 8-Webapp/8-2Flask/8-2-1flaskmvc/flask-boilerplate/app/models.py
@@ -0,0 +1,12 @@
+# from flask_mongoengine import MongoEngine
+
+# db=MongoEngine()
+from doctorappointmentapp import app,db 
+#from sqlalchemy.dialects.postgresql import JSON
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import *
+
+
+#create database model 
